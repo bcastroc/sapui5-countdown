a5e122131205878ea4fd18570b51442581107fd4
bcastro23 <bernal.castro@gmail.com>
2021-12-17 16:55:37 -0600

    New view for countdown until xmas

--- uimodule/webapp/controller/countdown/Countdown.controller.js
+++ uimodule/webapp/controller/countdown/Countdown.controller.js
@@ -8,15 +8,37 @@ sap.ui.define([
 
         return Controller.extend("local.myUI5App.controller.countdown.Countdown", {
             
-            onInit: function () {
+            onInit: function () {                
+
+                //let myTimer = this.calculateTime() ;
+
+                this.myTimer = {"days":0, "hours":0, "minutes":0, "seconds":0};
                 
-                // const mytilesModel = new JSONModel("../model/tiles.json");
-                // this.getView().setModel(mytilesModel, "tiles");
+                const mytilesModel = new JSONModel(this.myTimer);                
+                this.getView().setModel(mytilesModel, "timerModel");
+
+                setInterval(this.calculateTime.bind(this), 1000);
                 
             },
 
             onPress: function (oRoute) {
                 alert("Hello " + oRoute);
+            },
+
+            calculateTime: function () {
+                const currentDate = new Date();
+                const xmasDay = new Date("Dec 24 " + currentDate.getFullYear().toString() );                
+                const diff = xmasDay.getTime() - currentDate.getTime();
+
+                const timer = {
+                    "days"      : Math.floor(diff / ( 1000 * 60 * 60 * 24 )),
+                    "hours"     : Math.floor((diff % ( 1000 * 60 * 60 * 24 )) / ( 1000 * 60 * 60 )),
+                    "minutes"   : Math.floor((diff % ( 1000 * 60 * 60 ))/ ( 1000 * 60  ) ),
+                    "seconds"   : Math.floor((diff % ( 1000 * 60  ))/ ( 1000  ) )
+                };
+
+                this.getView().getModel("timerModel").setData(timer);
+                
             }
         });
     });
